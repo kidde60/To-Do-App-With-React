a66a6ebe09e350adb320a0ab724f28011f47d4c3
kidde60 <kgeorgewilliam60@gmail.com>
2022-09-24 21:37:23 +0300

    Complete app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import TodoContainer from "./components/TodoContainer"
-import "./App.css"
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// component
+import TodoContainer from './functionBased/components/TodoContainer';
+
+// stylesheet
+import './functionBased/App.css';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <TodoContainer />
-    </React.StrictMode>,
-    document.getElementById("root")
-)
+  <React.StrictMode>
+    <Router basename={process.env.PUBLIC_URL}>
+      <TodoContainer />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
